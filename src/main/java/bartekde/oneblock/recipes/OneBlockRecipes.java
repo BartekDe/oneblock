f00e134d4fb1aaef53befe3fe550dfa57bbfb6fd
BartekDe <bartekduda01@gmail.com>
2018-07-30 18:41:44 +0200

    clean up and added a furnace recipe

---
+++ src/main/java/bartekde/oneblock/recipes/OneBlockRecipes.java
@@ -0,0 +1,14 @@
+package bartekde.oneblock;
+
+import bartekde.oneblock.init.MyItems;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+public class OneBlockRecipes {
+
+    public static void initSmelting() {
+        GameRegistry.addSmelting(Items.CARROT, new ItemStack(MyItems.BASIC_ITEM), 4);
+    }
+
+}
